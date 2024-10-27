ef17a923bbea4b3376861bae5308ef4a8dd4f50c
Sohum Berdia <ssberdia@gmail.com>
2024-10-27 00:13:37 -0700

    TrustScore Is Displayed (Flawed Logic)

--- backend/Chrome Extension/server.js
+++ backend/Chrome Extension/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const cors = require('cors');
+import express, { json } from 'express';
+import cors from 'cors';
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
-app.use(express.json());
+app.use(json());
 
 // Endpoint to analyze products
 app.post('/analyze', async (req, res) => {
